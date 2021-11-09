aa2e2e16a0fb11ffc9a05774cab1ae8313680a9e
Felipe Navas <felipenavas.itec@gmail.com>
2021-11-09 15:52:32 -0300

    Adding create parent folder of fileUpload

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -8,9 +8,19 @@ const cargarArchivo = async(req = request, res = response) => {
       return;
     };
 
-    const nombre = await subirArchivo( req.files );
-
-    res.json({ nombre });
+    try {
+      // Para crear un nuevo directorio dentro de uploads:      
+      // const nombre = await subirArchivo( req.files, ['txt', 'md'], 'textos' );
+      
+      // Subo las imagenes dentro de uploads/img, le paso el undefined ya que tengo en el helper
+      // las extensionesValidas donde incluyo a las imagenes
+      const nombre = await subirArchivo( req.files, undefined, 'img' );
+  
+      res.json({ nombre });
+    } catch (error) {
+      console.log( error );
+      res.status(400).json({ msg: error});
+    };
 };
 
 
